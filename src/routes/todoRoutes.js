e26209a134ca65601dd6952606137870a7813aa0
Harsh Singh <Singhharsh0475@gmail.com>
2024-12-30 11:45:11 +0530

    completed the toDo_app with sqlite

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -13,11 +13,26 @@ router.get('/',(req,res)=>{
 })
 
 router.post('/',(req,res)=>{
-    
+    const {task}=req.body;
+    const insertTodo=db.prepare('INSERT INTO todos (user_id,task) VALUES (?,?)');
+    const result=insertTodo.run(req.userId,task);
+    res.status(201).json({message:'Todo added',id:result.lastInsertRowid,task,completed:0});
 })
 
-router.put('/:id',(req,res)=>{})
+router.put('/:id',(req,res)=>{
+    const {id}=req.params;
+    const {task,completed}=req.body;
+    const putTodo=db.prepare(`UPDATE todos SET completed=? WHERE id=?`);
+    putTodo.run(completed,id);
 
-router.delete('/:id',(req,res)=>{})
+    res.json({"message":'Todo completed'})
+})
+
+router.delete('/:id',(req,res)=>{
+    const {id}=req.params;
+    const deleteTodo=db.prepare('DELETE FROM todos WHERE id=? AND user_id=?');
+    deleteTodo.run(id,req.userId);
+    res.json({"message":'Todo deleted'});
+})
 
 export default router;
